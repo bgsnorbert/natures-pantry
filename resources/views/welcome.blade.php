57e245b66112e3e079f6e1069a10bae99eb6995a
Bogsch Norbert <Norbi@DESKTOP-AQNI504>
2024-08-20 00:28:02 +0300

    changes to workflow file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nature's Pantry - Home Page</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
