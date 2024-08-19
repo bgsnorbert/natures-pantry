6be0e13695ad475d8b6e42de64f9e40996835174
Bogsch Norbert <Norbi@DESKTOP-AQNI504>
2024-08-20 00:26:18 +0300

    changes to workflow file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            output: "/app/public/build",
             refresh: true,
         }),
     ],
