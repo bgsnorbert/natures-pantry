ef609f26f3cd571fd7ef17c82708f75386f98b2d
Bogsch Norbert <bogschnorbert@yahoo.com>
2024-08-20 22:53:03 +0300

    small code refactor on props, fails on production somehow

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,3 +1,6 @@
-<x-layout title="Nature's Pantry - Product">
+<x-layout>
+    <x-slot:page_title>
+        Nature's Pantry - Product
+    </x-slot:page_title>
     <div class="mt-5 ">Single product page</div>
 </x-layout>
